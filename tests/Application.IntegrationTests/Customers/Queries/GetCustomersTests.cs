f0f4c898e750a4a79387989c8dd669003d4555d1
Mehdi Nemati <mhdx92@gmail.com>
2023-06-15 21:25:41 +0330

    Application test - query

---
+++ tests/Application.IntegrationTests/Customers/Queries/GetCustomersTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using CustomerAccount.Application.Common.Mapping;
+using CustomerAccount.Application.Customers.Queries.Customers;
+using CustomerAccount.Domain.Entities;
+using CustomerAccount.Infrastructure.Persistence;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+
+namespace Application.IntegrationTests.Customers.Queries;
+internal class GetCustomersTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly IMapper mapper;
+    public ApplicationDbContext context;
+
+    public GetCustomersTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+        mapper = config.CreateMapper();
+        context = MockDatabase.InitDatabase(_mediatorMock);
+    }
+
+    [Test]
+    public async Task ShouldReturnItem()
+    {
+        context.Add(new Customer
+        {
+            Address = "Norway",
+            FullName = "Mehdi Nemati",
+            NationalCode = "123",
+            WalletBalance = 10
+        });
+        context.SaveChanges();
+
+        var handler = new GetCustomersQueryHandler(context, mapper);
+        var result = await handler.Handle(new GetCustomersQuery(), CancellationToken.None);
+
+        result.ToList().Should().NotBeEmpty();
+    }
+
+    [Test]
+    public async Task ShouldReturnZeroItem()
+    {
+        var handler = new GetCustomersQueryHandler(context, mapper);
+        var result = await handler.Handle(new GetCustomersQuery(), CancellationToken.None);
+
+        result.ToList().Should().BeEmpty();
+    }
+
+    [Test]
+    public async Task ShouldReturnItemThatNameContainMehdi()
+    {
+        context.Add(new Customer
+        {
+            Address = "Norway",
+            FullName = "Mehdi Nemati",
+            NationalCode = "123",
+            WalletBalance = 10
+        });
+        context.Add(new Customer
+        {
+            Address = "Italia",
+            FullName = "Jack Nemati",
+            NationalCode = "3365",
+            WalletBalance = 100
+        });
+        context.SaveChanges();
+
+
+        var handler = new GetCustomersQueryHandler(context, mapper);
+        var result = await handler.Handle(new GetCustomersQuery()
+        {
+            SearchText = "Mehdi"
+        }, CancellationToken.None);
+
+        result.ToList().Should().HaveCount(1);
+    }
+}
---
+++ tests/Application.IntegrationTests/MockDatabase.cs
@@ -0,0 +1,21 @@
+﻿using CustomerAccount.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Application.IntegrationTests;
+
+public static class MockDatabase
+{
+    public static ApplicationDbContext InitDatabase(Mock<IMediator> _mediatorMock)
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        var context = new ApplicationDbContext(options, _mediatorMock.Object);
+
+        return context;
+    }
+}
+
